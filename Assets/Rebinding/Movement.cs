33669e0225dbf65cf423729048d43d82a03d775e
Tim de Moor <timdemoor122@gmail.com>
2023-11-29 14:47:07 +0100

    Ported Example to 2D

--- Assets/Rebinding/Movement.cs
+++ Assets/Rebinding/Movement.cs
@@ -9,15 +9,14 @@ public class Movement : MonoBehaviour
 {
     private DefaultControls input;
     private Vector2 moveDirection = Vector2.zero;
-    private Rigidbody rb;
-    private float speed = 10f;
+    private Rigidbody2D rb;
+    private float speed = 100f;
 
     private void Awake()
     {
         input = new DefaultControls();
         input.LoadBindingOverridesFromJson(PlayerPrefs.GetString("SavedControls"));
-        Debug.Log(PlayerPrefs.GetString("SavedControls"));
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody2D>();
     }
     
     private void OnEnable()
@@ -42,7 +41,6 @@ public class Movement : MonoBehaviour
     private void OnMovementPerformed(InputAction.CallbackContext value)
     {
         moveDirection = value.ReadValue<Vector2>();
-        
     }
 
     private void OnMovementCanceled(InputAction.CallbackContext value)
